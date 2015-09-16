ecfe8bcd0e9990a3b643151828103fb9cc16fca0
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-09-16 15:48:32 +0200

    report status back via statuses api

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
+var request = require('request');
 var githubhook = require('githubhook');
 var github = githubhook();
+
+
+
+var XKCD_MAX = 1578;
+
+
 
 github.listen();
 
@@ -10,4 +17,56 @@ github.on('pull_request', function(repo, ref, data) {
   console.log(ref);
   console.log(data);
   console.log('start####################');
+
+  pending(data.pull_request.statuses_url);
+  setTimeout(function() {
+    success(data.pull_request.statuses_url, function(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+      console.log(body);
+    });
+  }, 1000 * 30);
 });
+
+
+
+function response(url, state, done) {
+  request({
+    url: url,
+    method: 'POST',
+    json: {
+      state: state,
+      target_url: 'http://xkcd.com/' + getRandomInt(1, XKCD_MAX),
+      description: 'The build succeeded!',
+      context: 'continuous-integration/maja'
+    }
+  }, done);
+}
+
+function success(url, done) {
+  return response(url, 'success', done);
+}
+
+function pending(url, done) {
+  return response(url, 'pending', done);
+}
+
+function error(url, done) {
+  return response(url, 'error', done);
+}
+
+function failure(url, done) {
+  return response(url, 'failure', done);
+}
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ * http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
