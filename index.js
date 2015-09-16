1302cb8331daf4d3432c3eeaa1796ba585914ac4
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-09-16 14:22:45 +0200

    make changes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var github = githubhook();
 
 github.listen();
 
-github.on('pull_request', function (data) {
-  console.log('got data: --------');
+github.on('pull_request', function(repo, ref, data) {
+  console.log('start####################');
+  console.log(repo);
+  console.log(ref);
   console.log(data);
   console.log('end-------------------');
 });
